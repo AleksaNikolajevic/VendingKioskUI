2a5dda5a85a8f72d7569399656acfb20764d2aa6
AleksaNikolajevic <75322640+AleksaNikolajevic@users.noreply.github.com>
2025-10-17 12:06:59 +0200

    Add project files.

---
+++ TagStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingKioskUI
+{
+    public class TagStatus
+    {
+        public string EPC { get; set; }
+        public string TID { get; set; }
+        public string Antenna { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public DateTime LastSeen { get; set; }
+        public int MissCount { get; set; } = 0;
+
+        public override bool Equals(object obj) => obj is TagStatus other && this.EPC == other.EPC;
+        public override int GetHashCode() => EPC.GetHashCode();
+    }
+}
